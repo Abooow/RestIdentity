4c1279b427932770becc457045428e43ec507ab9
Daniel Abdulahad <daniel00.abdulahad@gmail.com>
2021-07-14 20:29:14 +0200

    Added MudBlazor and themeing

---
+++ src/Client/ColorThemes/ThemeProvider.cs
@@ -0,0 +1,44 @@
+﻿using MudBlazor;
+using System;
+
+namespace RestIdentity.Client.ColorThemes
+{
+    public class ThemeProvider
+    {
+        public MudTheme Theme 
+        {
+            get => _theme; 
+            set
+            {
+                if (value is null || value == _theme)
+                    return;
+
+                OnThemeChanged?.Invoke(value);
+                _theme = value;
+            }
+        }
+        private MudTheme _theme;
+
+        public event Action<MudTheme> OnThemeChanged;
+
+        public ThemeProvider()
+        {
+            Theme = Themes.DarkTheme;
+        }
+
+        public void InvertTheme()
+        {
+            Theme = Theme == Themes.DarkTheme ? Themes.LightTheme : Themes.DarkTheme;
+        }
+
+        public void DarkTheme()
+        {
+            Theme = Themes.DarkTheme;
+        }
+
+        public void LightTheme()
+        {
+            Theme = Themes.LightTheme;
+        }
+    }
+}
--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,11 +1,8 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+using MudBlazor.Services;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace RestIdentity.Client
@@ -18,6 +15,7 @@ namespace RestIdentity.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
