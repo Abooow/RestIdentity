e00ba1d0daa52165d8b730ef4bd133901b7b0e9c
Daniel Abdulahad <daniel00.abdulahad@gmail.com>
2021-08-19 19:06:52 +0200

    Added logging with Serilog

---
+++ RestIdentity.Logging/TextFormatter.cs
@@ -0,0 +1,36 @@
+﻿using Serilog.Events;
+using Serilog.Formatting;
+
+namespace RestIdentity.Logging;
+
+public sealed class TextFormatter : ITextFormatter
+{
+    public void Format(LogEvent logEvent, TextWriter output)
+    {
+        if (logEvent.Level == LogEventLevel.Information)
+            return;
+
+        output.WriteLine(".......................................................");
+        output.WriteLine($"[{logEvent.Timestamp:dd/MMM/yyyy HH:mm:ss.FFF}] - {logEvent.Level}");
+
+        foreach (var property in logEvent.Properties)
+        {
+            output.WriteLine($"    {property.Key} : {property.Value}");
+        }
+
+        if (logEvent.Exception is not null)
+        {
+            output.WriteLine();
+            output.WriteLine("~~~~~~~~~~~~~~~~ EXCEPTION DETAILS ~~~~~~~~~~~~~~~~");
+            output.WriteLine($"    EXCEPTION - {logEvent.Exception}");
+            output.WriteLine($"    STACK TRACE - {logEvent.Exception.StackTrace}");
+            output.WriteLine($"    MESSAGE - {logEvent.Exception.Message}");
+            output.WriteLine($"    SOURCE - {logEvent.Exception.Source}");
+            output.WriteLine($"    INNER EXCEPTION - {logEvent.Exception.InnerException}");
+        }
+
+        output.WriteLine(".......................................................");
+        output.WriteLine();
+
+    }
+}
--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+﻿using RestIdentity.Logging;
+using Serilog;
+using System.Diagnostics;
 
 namespace RestIdentity.Server
 {
@@ -7,13 +8,24 @@ namespace RestIdentity.Server
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            IHost host = CreateHostBuilder(args).Build();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseSerilog((hotstingContext, loggongConfiguration) => loggongConfiguration
+                    .Enrich.FromLogContext()
+                    .Enrich.WithProperty("    Application", "RestIdentity")
+                    .Enrich.WithProperty("    CurrentManagedThreadId", Environment.CurrentManagedThreadId)
+                    .Enrich.WithProperty("    ProcessId", Environment.ProcessId)
+                    .Enrich.WithProperty("    ProcessName", Process.GetCurrentProcess().ProcessName)
+                    .WriteTo.File(new TextFormatter(), Path.Combine($@"{hotstingContext.HostingEnvironment.ContentRootPath}\Logs\", $"{DateTime.Now:yyyyMMdd_HHmmss}.txt"))
+                    .ReadFrom.Configuration(hotstingContext.Configuration));
+
                     webBuilder.UseStartup<Startup>();
                 });
     }
